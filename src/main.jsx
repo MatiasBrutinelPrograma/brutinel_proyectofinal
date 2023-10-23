b12eacf186243a3f0532f05090574b44a3b6f16a
Matias Brutinel <131211925+MatiasBrutinelPrograma@users.noreply.github.com>
2023-10-23 09:31:57 -0300

    estilos

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import './App.css'
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
